951982e8fac256aab74b8eec1c746e94db353f19
Leon Coto <leoncoto@gmail.com>
2012-07-22 11:51:33 +0100

    World ready. Out for Sunday roast now :D

--- www/js/games/bananabomber.js
+++ www/js/games/bananabomber.js
@@ -8,10 +8,26 @@ define(function (require) {
 
     Crafty.init(tileSize * worldWidth, tileSize * worldHeight);
 
+    function makeFlower(i, j) {
+        return Crafty.e('2D, DOM, flower, solid, SpriteAnimation, explodable')
+            .attr({
+                x: i * tileSize,
+                y: j * tileSize,
+                z: 1000
+            })
+            .animate('wind', 0, 1, 3)
+            .animate('wind', 80, -1)
+            .bind('explode', function () {
+                this.destroy();
+            });
+    }
+
     function generateWorld() {
-        var i, j, grass;
+        var i, j, grass, bush, flower;
         for (i = 0; i < worldWidth; i += 1) {
             for (j = 0; j < worldHeight; j += 1) {
+
+                // Grass
                 grass = 'grass' + Crafty.math.randomInt(1, 4);
                 Crafty.e('2D, DOM, ' + grass).
                     attr({
@@ -19,6 +35,38 @@ define(function (require) {
                         y: j * tileSize,
                         z: 1
                     });
+
+                // Fence of bushes
+                if (i === 0 || i === 24 || j === 0 || j === 20) {
+                    bush = 'bush' + Crafty.math.randomInt(1, 2);
+                    Crafty.e('2D, DOM, solid, ' + bush).
+                        attr({
+                            x: i * tileSize,
+                            y: j * tileSize,
+                            z: 2
+                        });
+                }
+
+                // Nice flowers
+                if (i > 0
+                        && i < 24
+                        && j > 0
+                        && j < 20
+                        && (Crafty.math.randomInt(0, 50) > 30)
+                        && !(i === 1 && j >= 16)
+                        && !(i === 23 && j <= 4)) {
+                    flower = makeFlower(i, j);
+                }
+
+                // Grid of bushes
+                if ((i % 2 === 0) && (j % 2 === 0)) {
+                    Crafty.e('2D, DOM, solid, bush1')
+                        .attr({
+                            x: i * tileSize,
+                            y: i * tileSize,
+                            z: 2000
+                        });
+                }
             }
         }
     }
